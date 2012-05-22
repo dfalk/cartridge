ef4f3a5feea03dafc7a1654e2bfbae3fc220cb92
Dmitry Falk <tfalc@mail.ru>
2012-05-22 14:34:23 +0400

    update ru translation

--- cartridge/shop/models.py
+++ cartridge/shop/models.py
@@ -157,7 +157,7 @@ class Product(Displayable, Priced, RichText):
     categories = models.ManyToManyField("Category",
                                         verbose_name=_("Product categories"),
                                         blank=True, related_name="products")
-    position = models.PositiveIntegerField(default=99)
+    position = models.PositiveIntegerField(_("Position"), default=99)
     vendor = models.ForeignKey("Vendor", verbose_name=_("Vendor"), blank=True, null=True)
     second_hand = models.BooleanField(_("Second hand"), default=False)
     date_added = models.DateTimeField(_("Date added"), auto_now_add=True,
