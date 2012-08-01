761028aecd2b0720858dc92edcb49a1cf6b4c4a4
Dmitry Falk <tfalc@mail.ru>
2012-08-01 11:48:52 +0400

    reorganize product colors injection

--- cartridge/shop/page_processors.py
+++ cartridge/shop/page_processors.py
@@ -14,16 +14,8 @@ def category_processor(request, page):
     Add paging/sorting to the products for the category.
     """
     settings.use_editable()
-
-    product_options = ProductOption.objects.all()
-    dict_options = {}
-    for opt in product_options:
-        dict_options[opt.name] = opt.image
-
     products = Product.objects.published(for_user=request.user
                                 ).filter(page.category.filters()).distinct()
-
-
     sort_options = [(slugify(option[0]), option[1])
                     for option in settings.SHOP_PRODUCT_SORT_OPTIONS]
     sort_by = request.GET.get("sort", sort_options[0][1])
@@ -36,6 +28,11 @@ def category_processor(request, page):
                         settings.SHOP_PER_PAGE_CATEGORY,
                         settings.MAX_PAGING_LINKS)
     products.sort_by = sort_by
+    # inject product colors:
+    product_options = ProductOption.objects.all()
+    dict_options = {}
+    for opt in product_options:
+        dict_options[opt.name] = opt.image
     for product in products.object_list:
         product.colors = []
         for variation in product.variations.all():
@@ -44,5 +41,4 @@ def category_processor(request, page):
                     product.colors.append(dict_options[variation.option1])
             except:
                 pass
-        product.save()
     return {"products": products}
